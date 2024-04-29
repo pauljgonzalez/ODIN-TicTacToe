e9403fe07fbf3abfb489a0d6a9cb5983ac87a583
Paul <159176858+pauljgonzalez@users.noreply.github.com>
2024-04-29 14:26:23 -0400

    Layout and functionality complete.
    Might come back later to revisit
    and check for any bugs/ refactor code

--- srcipts.js
+++ srcipts.js
@@ -1,18 +1,9 @@
-        //if count equal max count it is a draw, figure out max count
-        //console.log("CHECKWINNER TEST");
-        //console.log(board.getBoard()[0][0].getValue());
-        //console.log(board.getBoard()[0].length + " row length"); horizontal length
-       // console.log(board.getBoard().length + " column length"); vertical length
-        //console.log("CHECKWINNER TEST");
-
-
-
 function Gameboard(){
     //initaites rows, columns, and gameboard
+    const size = 3;
     const rows = 3;
     const columns = 3;
     const gameboard = [];
-
     //sets gameboard grid based on rows and columns
     for (let i = 0; i < columns; i++){
         gameboard[i] = [];
@@ -27,28 +18,21 @@ function Gameboard(){
         const boardWithCellValues = gameboard.map((row) => row.map((cell) => cell.getValue()))
         console.log(boardWithCellValues);
     }
-
     //place the current players token by getting the column, row, and player
     //player is equal to the active players token value
     const placeToken =(column,row, player) =>{
-        if(gameboard[column][row].getValue() === 0){
+        if(gameboard[column][row].getValue() === ""){
             gameboard[column][row].getToken(player);
         }
         else{
             return "False";
         }
     }
-
     return {getBoard,printBoard,placeToken}
 }
-
-
-
-
 function Cell(){
     //iniation values is 0
-    let value = 0;
-
+    let value = "";
     const getValue = () => value;
     //get players token value by being passed the current active players token
     const getToken = (player) =>{
@@ -57,19 +41,13 @@ function Cell(){
     return {getValue,getToken};
 
 }
-
-
-
-
 function GameController(
     playerOneName = "Player One",
     playerTwoName = "Player Two"
 ){
-
+    let count = 0;
     const board = Gameboard()
     const getBoard = board.getBoard();
-    // count is to iniate when to check for a winner/draw
-    let count = 5;
     const players = [
         {
             name: playerOneName,
@@ -91,20 +69,16 @@ function GameController(
             activePlayer = players[0];
         }
     }
-
     const getActivePlayer = () => activePlayer;
     // don't think this is needed
     //const getNewRound = board.getBoard()
-
     //prints new round and tells the console what players turn it is
     const printNewRound = () =>{
         board.printBoard()
         console.log(getActivePlayer().name + "'s Turn")
     }
-   
 
     const checkWinner = () =>{
-        
        //check columns for winner
        for(let i = 0; i < getBoard[0].length; i ++){
             if(checkColumns(i)){
@@ -127,10 +101,13 @@ function GameController(
             if(checkTopRightDiagonal(2)){
                 return getBoard[0][2].getValue();
             }
+            if(count === 8){
+                return "Draw"
+            }
     }
     const checkColumns = (column) =>{
         const firstCell = getBoard[0][column].getValue();
-        if(firstCell == 0){
+        if(firstCell == ""){
             return false;
         }
         for(let i = 1; i < getBoard.length;i++){
@@ -142,7 +119,7 @@ function GameController(
     }
     const checkRows = (row) =>{
         const firstCell = getBoard[row][0].getValue();
-        if(firstCell == 0){
+        if(firstCell == ""){
             return false;
         }
         for(let i = 1; i < getBoard[0].length;i++){
@@ -154,7 +131,7 @@ function GameController(
     }
     const checkTopLeftDiagonal = (diagonal) =>{
         const firstCell = getBoard[diagonal][diagonal].getValue();
-        if(firstCell == 0){
+        if(firstCell == ""){
             return false
         }
         for(let i = 1; i < getBoard[0].length;i++){
@@ -166,44 +143,91 @@ function GameController(
     }
     const checkTopRightDiagonal = (diagonal) =>{
        const firstCell = getBoard[0][diagonal].getValue();
-       if(firstCell == 0){
+       if(firstCell == ""){
         return false;
        }
        if(firstCell == getBoard[1][1].getValue() && firstCell == getBoard[diagonal][0].getValue()){
         return true;
        }
     }
-
     const playRound = (column,row) => {
         // checks for return false
         const checkValue =  board.placeToken(column,row,getActivePlayer().token);
         if (checkValue === "False"){
             console.log("Invalid Move. Space Occupied.");
-            switchPlayer();
+            playRound(column,row);
         }
         else{
             board.placeToken(column,row,getActivePlayer().token);
         }
-        count ++;
+       
 
-        if (count >= 5){
-            if(checkWinner()){
-                let winner = checkWinner();
-                board.printBoard()
-                return "Winner is "+ winner
+        if(checkWinner() === "X" || checkWinner() ==="O"){
+             let winningToken = checkWinner();
+             let winningName;
+            if(players[0].token == winningToken){
+                winningName = players[0].name;
             }
-        } 
-
+            else{
+                winningName = players[1].name;
+            }
+            board.printBoard();
+            document.getElementById("winner").style.visibility = "visible";
+            document.querySelector(".displayWinner").innerHTML = winningName + " wins";
+         }
+         if (checkWinner() ==="Draw"){
+            board.printBoard();
+            document.getElementById("winner").style.visibility = "visible";
+            document.querySelector(".displayWinner").innerHTML = "Draw";
+         }
         switchPlayer();
         printNewRound();
+        count ++;
     }
-
-
     //iniates first round
     printNewRound()
-
-    return{playRound,getActivePlayer}
-
+    return{playRound,getActivePlayer};
 }
 
-const game = GameController();
+let play;
+
+document.querySelector(".submit button").onclick = function(){
+    let playerOneName = document.getElementById("playerOne").value;;
+    let playerTwoName = document.getElementById("playerTwo").value;; 
+    if (playerOneName === "" ){
+        playerOneName = "Player One";
+    }
+    if (playerTwoName === "" ){
+        playerTwoName = "Player Two";
+    }
+    console.log(playerTwoName)
+    play = GameController(playerOneName,playerTwoName);
+    document.querySelector(".submit button").style.visibility = "hidden";
+}
+
+document.querySelector("#winner button").onclick = function(){
+    let winner  = document.getElementById("winner");
+   
+    let list = document.querySelectorAll(".class");
+    list.forEach((item) => {
+        item.innerHTML = "";
+    });
+    
+    play = GameController(document.getElementById("playerOne").value,document.getElementById("playerTwo").value);
+    document.querySelector("#winner").style.visibility = "hidden";
+}
+
+
+// apply the event listener to ids
+
+document.querySelectorAll('.class').forEach((id) => {
+    id.addEventListener('click', event => {
+      const id = event.currentTarget.id;
+      let text = play.getActivePlayer().token
+      document.getElementById(id).innerHTML;
+      play.playRound(id[0],id[1]);
+      document.getElementById(id).innerHTML = text;
+    });
+  });
+
+ 
